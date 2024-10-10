aeb852ecad2402e931cffc37aa2701bc567101d4
Carlos Genuino <carloxsgenuino3@live.com>
2024-10-10 15:59:50 -0300

    adicionando o cluster e a criacao dos nodes

--- variables.tf
+++ variables.tf
@@ -9,3 +9,25 @@ variable "cluster_name" {
   default     = "curso-terraform"
   description = "nome do cluster"
 }
+
+variable "retention_in_days" {
+  type        = number
+  default     = 30
+  description = "tempo de retenção em dias"
+}
+
+variable "desired_size" {
+  type        = number
+  default     = 2
+  description = "quantidade de maquinas desejadas"
+}
+variable "max_size" {
+  type        = number
+  default     = 4
+  description = "quantidade maxima de maquinas desejadas"
+}
+variable "min_size" {
+  type        = number
+  default     = 2
+  description = "quantidade minima de maquinas desejadas"
+}
